47ff0786eb358385e48b4bcc44d8f4ca9ebf9a9d
Max Ehnert <max.ehnert@gmail.com>
2014-10-07 18:57:00 -0400

    eod commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,23 +6,23 @@ api.results.forEach(function (a){
 
 ////Build item description
 
-  list_description =  "<p>" + a.title + "</p>";
+  list_description =  "<p class='truncate'>" + a.title + "</p>";
 
 ////Build item price
 
-  list_price = "<p>" + a.price + "</p>";
+  list_price = "<span>" + a.price + "</span>";
 
 ////Build item currency code
 
-  list_currency = "<p>" + a.currency_code + "</p>";
+  list_currency = "<span>" + a.currency_code + "</span>";
 
 ////Build item seller
 
-  list_seller = "<p>" + a.Shop.shop_name + "</p>"
+  list_seller = "<span>" + a.Shop.shop_name + "</span>"
 
       a.Images.forEach(function(b){
 
-        list_image = "<img src='" + b.url_fullxfull + "' />'";
+        list_image = "<img src='" + b.url_fullxfull + "' />";
 
       });
 
