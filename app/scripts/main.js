a40f799d5581ea4dd14529c1048156545daf86cb
Max Ehnert <max.ehnert@gmail.com>
2014-11-21 14:14:49 -0500

    deploying

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,40 @@
 
-var container = $('#item'), list_price, list_currency, list_description, list_seller, list_image;
+var container = $('#item'), list_price, list_currency, list_description, list_seller, list_image, images;
 
 api.results.forEach(function (a){
 
 
 ////Build item description
 
-  list_description =  "<p class='truncate'>" + a.title + "</p>";
+  list_description =  "<a href='#'><p class='truncate'>" + a.title + "</p></a>";
 
 ////Build item price
 
-  list_price = "<span>" + a.price + "</span>";
+  list_price = "<span class='price'>" + '$' + a.price + "</span>";
 
 ////Build item currency code
 
-  list_currency = "<span>" + a.currency_code + "</span>";
+  list_currency = "<span class='currency'>" + a.currency_code + "</span>";
 
 ////Build item seller
+      images = $(images);
 
-  list_seller = "<span>" + a.Shop.shop_name + "</span>"
+  list_seller = "<a href='#'><span class='seller'>" + a.Shop.shop_name + "</span></a>"
 
       a.Images.forEach(function(b){
 
-        list_image = "<img src='" + b.url_fullxfull + "' />";
+        list_image = "<a href='#'> <img src='" + b.url_fullxfull + "' /></a>";
 
       });
 
-  items = "<li>" + list_image + list_description + list_seller + list_price + list_currency + "</li>";
+$('img').hover(
+
+  function () {
+    $(this).toggleClass('showButtons');
+  }
+
+);
+
+  items = "<li>" + list_image + images + list_description + list_seller + list_currency + list_price + "</li>";
   $(horses).append(items);
 });
